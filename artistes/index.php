bbb7785bb48560079885e0efba862c69d0cafb34
Isaac Dubé <isaacdubecap35@outlook.com>
2022-10-18 17:22:41 -0400

    changement dans la nav

--- artistes/index.php
+++ artistes/index.php
@@ -1,9 +1,24 @@
 <?php $niveau= '../';
 
 include($niveau . 'inc/scripts/config.inc.php');
-include($niveau . 'inc/fragments/header.inc.php');
+
 ?>
 
 
+<!doctype html>
+<html lang="fr">
+<head>s
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Liste des artistes</title>
+</head>
 
-<h1>Liste</h1>
+<body>
+<?php include($niveau . 'inc/fragments/header.inc.php');; ?>
+
+<h1>Liste Artiste</h1>
+
+</body>
+</html>
--- inc/fragments/header.inc.php
+++ inc/fragments/header.inc.php
@@ -2,16 +2,13 @@
 <nav class="nav" aria-label="Menu principal">
     <ul class="nav__list" id="navList">
         <li class="nav__list-item">
-            <a href="<?php echo $niveau ?>p-accueil-prenom.php" class="nav__link nav__link--active" arria-current="page" >Accueil</a>
+            <a href="<?php echo $niveau ?>index.php" class="nav__link nav__link--active" arria-current="page" >Accueil</a>
         </li>
         <li class="nav__list-item">
-            <a href="<?php echo $niveau ?>artistes/p-liste-prenom.php" class="nav__link">Participants</a>
+            <a href="<?php echo $niveau ?>artistes/index.php" class="nav__link">Participants</a>
         </li>
         <li class="nav__list-item">
-            <a href="#" class="nav__link">Lien 4</a>
-        </li>
-        <li class="nav__list-item">
-            <a href="#" class="nav__link">Lien 5</a>
+            <a href="<?php echo $niveau ?>artistes/fiche/index.php" class="nav__link">Fiches</a>
         </li>
     </ul>
 </nav>
