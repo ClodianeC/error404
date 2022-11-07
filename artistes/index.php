1e637746f5b47a391fce09354f01b97da100faa9
Clodiane Charette <2164515@csfoy.ca>
2022-11-06 20:07:12 -0500

    ajout artistes vedettes, placement artistes vedettes ajout h2, modif h2->h3, changement aspect checkbox ajout styles à vedettes

--- artistes/index.php
+++ artistes/index.php
@@ -110,14 +110,44 @@ for($intCptStyle = 0; $rangee=$pdosResultat->fetch();$intCptStyle++){
     $arr_style[$intCptStyle]["nom_style"]=$rangee["nom_style"];
 }
 
+//Requete artistes complets
 $str_requeteArtisteComplet = "SELECT id_artiste, nom_artiste FROM t_artiste";
 $pdosResultat = $pdoConnexion->query($str_requeteArtisteComplet);
 $arr_artisteComplet = array();
 
-for($intCptStyle = 0; $rangee=$pdosResultat->fetch();$intCptStyle++){
-    $arr_artisteComplet[$intCptStyle]["id_artiste"]=$rangee["id_artiste"];
-    $arr_artisteComplet[$intCptStyle]["nom_artiste"]=$rangee["nom_artiste"];
+for($intCptArtistesC = 0; $rangee=$pdosResultat->fetch();$intCptArtistesC++){
+    $arr_artisteComplet[$intCptArtistesC]["id_artiste"]=$rangee["id_artiste"];
+    $arr_artisteComplet[$intCptArtistesC]["nom_artiste"]=$rangee["nom_artiste"];
+
+    //Établissement de la deuxième requête (pour les styles liés à l'artiste)
+    $strRequeteStyleV="SELECT nom_style FROM t_style INNER JOIN ti_style_artiste ON ti_style_artiste.id_style=t_style.id_style WHERE id_artiste=".$rangee['id_artiste'];
+
+    //Initialisation de PDOStatement avec la nouvelle requête
+    $pdosSousResultatV = $pdoConnexion->prepare($strRequeteStyleV);
+    $pdosSousResultatV->execute();
+
+    $ligneStyleV = $pdosSousResultatV->fetch();
+    $strStylesV="";
+
+    //Extraction des données de la BD (pour les styles)
+    for($intCptStyleV=0;$intCptStyleV<$pdosSousResultatV->rowCount();$intCptStyleV++){
+        if($strStylesV != ""){
+            $strStylesV = $strStylesV . ", ";
+        }
+        $strStylesV = $strStylesV . $ligneStyleV['nom_style'];
+        $ligneStyleV = $pdosSousResultatV->fetch();
+    }
+
+    //Libération de la requête Styles
+    $pdosSousResultatV->closeCursor();
+    //Création d'une section de tableau pour style
+    $arr_artisteComplet[$intCptArtistesC]['style_artiste'] = $strStylesV;
+
+//Nouvel artiste
+    $ligne=$pdosResultat->fetch();
 }
+//Libération de la requête Artistes complets
+$pdosResultat->closeCursor();
 
 $arr_randomArtisteSugg = [-1,-1,-1];
 for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $intCptRand++){
@@ -145,8 +175,6 @@ for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $i
 <div>
 <?php include($niveau . 'inc/fragments/header.inc.php'); ?>
 
-
-
 <div class="tri-filtres">
     <div class="section-tri">
         <h2 class="h2_tri">Trier :</h2>
@@ -160,7 +188,7 @@ for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $i
         </form>
     </div>
     <div class="section-filtre">
-        <h2 class="h2_tri">Filtrer par styles</h2>
+        <h2 class="h2_tri">Filtrer par styles :</h2>
         <form class="filtre_formulaire">
             <?php
             for($intCptFiltre = 0; $intCptFiltre < count($arr_style); $intCptFiltre++){
@@ -184,12 +212,10 @@ for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $i
             }
             ?>
         </h1>
+
     </div>
 </div>
-
-
-
-
+    <h2 class="h2 titrePage">Page <?php echo $id_page+1?></h2>
 <ul class="liste-artistes">
     <?php
     for($intCpt=0;$intCpt<count($arr_artiste);$intCpt++){
@@ -212,7 +238,7 @@ for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $i
             </a>
         </div>
         <div class="artiste_info">
-            <h2 class="artiste_info_nom <?php echo $str_classLongTitre ?>"><?php echo $arr_artiste[$intCpt]["nom_artiste"]?></h2>
+            <h3 class="artiste_info_nom <?php echo $str_classLongTitre ?>"><?php echo $arr_artiste[$intCpt]["nom_artiste"]?></h3>
             <p class="artiste_info_style"><?php echo $arr_artiste[$intCpt]["style_artiste"]?></p>
         </div>
 
@@ -221,7 +247,42 @@ for($intCptRand = 0; $intCptRand<3 && $intCptRand<count($arr_artisteComplet); $i
     }
     ?>
 </ul>
+    <h2 class="h2 titreVedette">En vedette</h2>
+
 <?php
+echo "<ul class='liste_artistes-vedettes'>";
+    for($intCptVedette = 0; $intCptVedette<3; $intCptVedette++){
+        if($arr_randomArtisteSugg[$intCptVedette] != -1){
+            $int_randFinal = $arr_randomArtisteSugg[$intCptVedette];
+            $str_queryVedette = "./fiche/index.php?id_artiste=".$arr_artisteComplet[$int_randFinal]["id_artiste"];
+            $str_classArtistePair = "pair";
+            $str_classLongTitre = "";
+            if($intCptVedette%2==0){
+                $str_classArtistePair = "impair";
+            }
+            if(strlen($arr_artisteComplet[$int_randFinal]["nom_artiste"])>12){
+                $str_classLongTitre = "longTitre";
+            }
+            ?>
+            <li class="artistes-vedettes <?php echo $str_classArtistePair?>">
+                <div class="artistes-vedettes_deco">
+                    <a class="artistes-vedettes_lien" href="<?php echo $str_queryVedette ?>">
+                        <picture class="artiste-vedettes_img">
+                            <?php echo "<img src='../images/liste-artistes/artistes/".$arr_artisteComplet[$int_randFinal]['id_artiste']."_w520.jpg' srcset='../images/liste-artistes/artistes/".$arr_artisteComplet[$int_randFinal]['id_artiste']."_w260.jpg 1x, ../images/liste-artistes/artistes/".$arr_artisteComplet[$int_randFinal]['id_artiste']."_w520.jpg 2x'>"; ?>
+                        </picture>
+                    </a>
+                </div>
+                <div class="artiste-vedettes_info">
+                    <h3 class="artiste-vedettes_info_nom <?php echo $str_classLongTitre ?>"><?php echo $arr_artisteComplet[$int_randFinal]["nom_artiste"]?></h3>
+                    <p class="artiste-vedettes_info_style"><?php echo $arr_artisteComplet[$int_randFinal]["style_artiste"]?></p>
+                </div>
+
+            </li>
+            <?php
+        }
+    }
+echo "</ul>";
+
 echo "<div class='controles_div_total'><div class='controles_deco'><div class='controle-page'>";
 if(isset($_GET["id_style"])){
     //Déterminer max page si filtre
