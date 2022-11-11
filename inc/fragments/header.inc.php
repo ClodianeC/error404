93ea2d2c7de0618a480129e36e2925d7f96baea9
Isaac Dubé <isaacdubecap35@outlook.com>
2022-11-11 06:58:42 -0500

    header

--- inc/fragments/header.inc.php
+++ inc/fragments/header.inc.php
@@ -1,14 +1,63 @@
-<a href="#contenu" class="visuallyhidden focusable">Aller au contenu</a>
-<nav class="nav" aria-label="Menu principal">
-    <ul class="nav__list" id="navList">
-        <li class="nav__list-item">
-            <a href="<?php echo $niveau ?>index.php" class="nav__link nav__link--active" arria-current="page" >Accueil</a>
-        </li>
-        <li class="nav__list-item">
-            <a href="<?php echo $niveau ?>artistes/index.php" class="nav__link">Participants</a>
-        </li>
-        <li class="nav__list-item">
-            <a href="<?php echo $niveau ?>artistes/fiche/index.php" class="nav__link">Fiches</a>
-        </li>
-    </ul>
-</nav>
+<!-- <a href="#contenu" class="visuallyhidden focusable">Aller au contenu</a> -->
+<header class="header">
+    <div class="header_nav">
+        <div class="image_carousel">
+            <!--        <picture class="picture__entete ">-->
+            <!--            <source media="(max-width: 640px)"-->
+            <!--                    srcset="accueil_table/DiamondRings_DSC0860.jpg 1x, images/image_en_tete_2x_1080-moyen.jpg 2x">-->
+            <!---->
+            <!--            <source media="(min-width: 641px)" srcset="images/image_en_tete_2x_2440-grand.jpg 2x">-->
+
+            <img id="imageCarousel" src="../../accueil_table/DiamondRings_DSC0731.jpg" alt="Diamond Ring sur la scène">
+        </div>
+        <!--        </picture>-->
+        <a href="../../index.php" class="header__nav--logo"><img class="img__nav--logo"
+                                                                 src="../../accueil_table/reseau_sociaux/logo.png" alt="Accueil">
+    </div>
+    <div class="nav-menu">
+        <nav class="nav--principale" aria-label="Menu principal">
+            <ul class="nav--principale__list header-nav" id="navPrincipaleList">
+                <li class="nav--principale__list-elem">
+                    <a href="#" class="nav--principale__link nav--principale__link--active"
+                       arria-current="page">Programmation</a>
+                </li>
+                <li class="nav--principale__list-elem">
+                    <a href="#" class="nav--principale__link">Participants</a>
+                </li>
+                <li class="nav--principale__list-elem">
+                    <a href="#" class="nav--principale__link">Contactez-nous</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="hamburger">
+        <div class="bubble">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </div>
+    </div>
+    <div class="header-socials">
+        <div class="socials-icons ">
+            <ul class="socials-icons__list header-icons">
+                <li class="socials-icons__list-elem"><a class="socials-icons-link" href="#"><img
+                                class="socials-icons-img" src="../../accueil_table/reseau_sociaux/facebook.png" alt=""></a>
+                </li>
+                <li class="socials-icons__list-elem"><a class="socials-icons-link" href="#"><img
+                                class="socials-icons-img" src="../../accueil_table/reseau_sociaux/youtube.png" alt=""></a>
+                </li>
+                <li class="socials-icons__list-elem"><a class="socials-icons-link" href="#"><img
+                                class="socials-icons-img" src="../../accueil_table/reseau_sociaux/twitter.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="bouton_carousel">
+        <button class="btn_carousel bouton_carousel-1" id="btn1" value="btn1"></button>
+        <button class="btn_carousel bouton_carousel-2" id="btn2" value="btn2"></button>
+        <button class="btn_carousel bouton_carousel-3" id="btn3" value="btn3"></button>
+    </div>
+</header>
+<link rel="stylesheet" href="/css/style-isaac.css">
+<script src="../../js/menu.js"></script>
--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php $niveau= '';
 
 include($niveau . 'inc/scripts/config.inc.php');
-include($niveau . 'inc/scripts/config.inc.php');
 
 ?>
 
 
 <!doctype html>
 <html lang="fr">
-<head>s
+<head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style-isaac.css">
     <title>Accueil</title>
 </head>
 
 <body>
+
 <?php include($niveau . 'inc/fragments/header.inc.php');; ?>
 
 <h1>Accueil</h1>
